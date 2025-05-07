29c45b6aec9613067207dfa6cc5d621dc6878d82
bini <jaletakebede@gmail.com>
2025-05-07 15:40:57 +0300

    Added new custom navigation link component

---
+++ Askify/src/components/shared/NavigationLink.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+interface Props {
+  to: string;
+  bg: string;
+  text: string;
+  textColor: string;
+  onClick?: () => Promise<void>;
+}
+const NavigationLink = ({ to, bg, text, textColor, onClick }: Props) => {
+  return (
+    <Link
+      onClick={onClick}
+      className="nav-link"
+      to={to}
+      style={{ background: bg, color: textColor }}
+    >
+      {text}
+    </Link>
+  );
+};
+
+export default NavigationLink;
